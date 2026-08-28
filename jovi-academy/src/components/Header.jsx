99ea8c2cb612c5d347cb9d5197fbc8450f148cee
Rafaella <raffa.pazza10@gmail.com>
2026-08-28 13:48:17 -0300

    Criação do projeto e componentização

---
+++ jovi-academy/src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="/">jovi academy.</a>
+
+        <ul>
+          <li><a href="#solucao">A Solução</a></li>
+          <li><a href="#publico">Público-Alvo</a></li>
+          <li><a href="#galeria">Galeria</a></li>
+          <li><a href="#equipe">Nossa Equipe</a></li>
+          <li><a href="#contato">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
